00f73c20bb802afeb29b92c16131ec85e23cc601
George Witteman <george@witteman.me>
2021-09-24 15:25:29 -0700

    Update DMARC for both emails

--- dnsimple.tf
+++ dnsimple.tf
@@ -1,13 +1,13 @@
 module "dnsimple_fastmail_georgewitteman_com" {
   source = "./modules/dnsimple-fastmail"
   domain = "georgewitteman.com"
-  dmarc  = "v=DMARC1; p=none; pct=100; rua=mailto:re+uimzzzgugvk@dmarc.postmarkapp.com; sp=none; aspf=r;"
+  dmarc  = "v=DMARC1; p=quarantine; sp=quarantine; aspf=r; pct=100; rua=mailto:re+uimzzzgugvk@dmarc.postmarkapp.com; ruf=mailto:forensicreports@witteman.me; fo=1:d:s"
 }
 
 module "dnsimple_fastmail_witteman_me" {
   source = "./modules/dnsimple-fastmail"
   domain = "witteman.me"
-  dmarc  = "v=DMARC1; p=none; sp=none; aspf=r; rua=mailto:dmarc_agg@vali.email,mailto:re+igrne0h0z9h@dmarc.postmarkapp.com; ruf=mailto:forensicreports@witteman.me; fo=1:d:s"
+  dmarc  = "v=DMARC1; p=quarantine; sp=quarantine; aspf=r; pct=100; rua=mailto:re+igrne0h0z9h@dmarc.postmarkapp.com; ruf=mailto:forensicreports@witteman.me; fo=1:d:s"
 }
 
 module "dnsimple_github_pages" {
