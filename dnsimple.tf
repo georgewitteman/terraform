85d06f8792617539080614a07f1ca3ede7c2a0f0
George Witteman <george@witteman.me>
2021-09-24 09:59:03 -0700

    Automated terraform apply

--- dnsimple.tf
+++ dnsimple.tf
@@ -10,23 +10,6 @@ module "dnsimple_fastmail_witteman_me" {
   dmarc  = "v=DMARC1; p=none; pct=100; rua=mailto:re+igrne0h0z9h@dmarc.postmarkapp.com; sp=none; aspf=r;"
 }
 
-module "dnsimple_fastmail_witteman_email" {
-  source = "./modules/dnsimple-fastmail"
-  domain = "witteman.email"
-  dmarc  = "v=DMARC1; p=none; pct=100; rua=mailto:re+nc4kkbmbvko@dmarc.postmarkapp.com; sp=none; aspf=r;"
-}
-
-module "dnsimple_fastmail_witteman_family" {
-  source = "./modules/dnsimple-fastmail"
-  domain = "witteman.family"
-  dmarc  = "v=DMARC1; p=none; pct=100; rua=mailto:re+yyvpwlplj2n@dmarc.postmarkapp.com; sp=none; aspf=r;"
-}
-
-module "dnsimple_fastmail_wtmn_net" {
-  source = "./modules/dnsimple-fastmail"
-  domain = "wtmn.net"
-}
-
 module "dnsimple_github_pages" {
   source       = "./modules/dnsimple-github-pages"
   domain       = "georgewitteman.com"
