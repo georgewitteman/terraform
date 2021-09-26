101162c7d2f831307e298aecf5bc6a412b0df62c
George Witteman <george@witteman.me>
2021-09-26 05:30:39 +0000

    Add gitpod-setup.sh

---
+++ gitpod-setup.sh
@@ -0,0 +1,6 @@
+#!/bin/sh -xe
+eval $(gp env --export)
+curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
+sudo apt-add-repository "deb [arch=$(dpkg --print-architecture)] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
+sudo apt install --yes terraform
+terraform init
