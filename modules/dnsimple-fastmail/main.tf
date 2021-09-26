70382d2e304ce6a01e7bcb218473e024a2428e49
George Witteman <george@witteman.me>
2021-09-26 05:35:39 +0000

    Switch from dnsimple_record to dnsimple_zone_record

--- modules/dnsimple-fastmail/main.tf
+++ modules/dnsimple-fastmail/main.tf
@@ -4,145 +4,145 @@ locals {
   # mx_hosts   = ["", "*", "mail"]
 }
 
-resource "dnsimple_record" "a" {
-  count  = var.a_records == "true" ? 1 : 0
-  domain = var.domain
-  name   = ""
-  type   = "ALIAS"
-  ttl    = var.ttl
+resource "dnsimple_zone_record" "a" {
+  count     = var.a_records == "true" ? 1 : 0
+  zone_name = var.domain
+  name      = ""
+  type      = "ALIAS"
+  ttl       = var.ttl
 
   value = "web.messagingengine.com"
 }
 
-resource "dnsimple_record" "a_wildcard" {
-  count  = var.a_records == "true" ? 1 : 0
-  domain = var.domain
-  name   = "*"
-  type   = "ALIAS"
-  ttl    = var.ttl
+resource "dnsimple_zone_record" "a_wildcard" {
+  count     = var.a_records == "true" ? 1 : 0
+  zone_name = var.domain
+  name      = "*"
+  type      = "ALIAS"
+  ttl       = var.ttl
 
   value = "web.messagingengine.com"
 }
 
-resource "dnsimple_record" "mail" {
-  domain = var.domain
-  name   = "mail"
-  type   = "ALIAS"
-  ttl    = var.ttl
+resource "dnsimple_zone_record" "mail" {
+  zone_name = var.domain
+  name      = "mail"
+  type      = "ALIAS"
+  ttl       = var.ttl
 
   value = "mail.fastmail.com"
 }
 
-resource "dnsimple_record" "mx_10" {
+resource "dnsimple_zone_record" "mx_10" {
   count = length(local.mx_hosts)
 
-  domain   = var.domain
-  name     = element(local.mx_hosts, count.index)
-  type     = "MX"
-  ttl      = var.ttl
-  priority = "10"
+  zone_name = var.domain
+  name      = element(local.mx_hosts, count.index)
+  type      = "MX"
+  ttl       = var.ttl
+  priority  = "10"
 
   value = "in1-smtp.messagingengine.com"
 }
 
-resource "dnsimple_record" "mx_20" {
+resource "dnsimple_zone_record" "mx_20" {
   count = length(local.mx_hosts)
 
-  domain   = var.domain
-  name     = element(local.mx_hosts, count.index)
-  type     = "MX"
-  ttl      = var.ttl
-  priority = "20"
+  zone_name = var.domain
+  name      = element(local.mx_hosts, count.index)
+  type      = "MX"
+  ttl       = var.ttl
+  priority  = "20"
 
   value = "in2-smtp.messagingengine.com"
 }
 
-resource "dnsimple_record" "spf" {
-  domain = var.domain
-  name   = ""
-  type   = "TXT"
-  ttl    = var.ttl
+resource "dnsimple_zone_record" "spf" {
+  zone_name = var.domain
+  name      = ""
+  type      = "TXT"
+  ttl       = var.ttl
 
   value = "v=spf1 include:spf.messagingengine.com -all"
 }
 
-resource "dnsimple_record" "dkim" {
+resource "dnsimple_zone_record" "dkim" {
   count = length(local.dkim_hosts)
 
-  domain = var.domain
-  name   = "${element(local.dkim_hosts, count.index)}._domainkey"
-  type   = "CNAME"
-  ttl    = var.ttl
+  zone_name = var.domain
+  name      = "${element(local.dkim_hosts, count.index)}._domainkey"
+  type      = "CNAME"
+  ttl       = var.ttl
 
   value = "${element(local.dkim_hosts, count.index)}.${var.domain}.dkim.fmhosted.com"
 }
 
-resource "dnsimple_record" "dmarc" {
-  count  = var.dmarc == "" ? 0 : 1
-  domain = var.domain
-  name   = "_dmarc"
-  type   = "TXT"
-  ttl    = var.ttl
+resource "dnsimple_zone_record" "dmarc" {
+  count     = var.dmarc == "" ? 0 : 1
+  zone_name = var.domain
+  name      = "_dmarc"
+  type      = "TXT"
+  ttl       = var.ttl
 
   value = var.dmarc
 }
 
-resource "dnsimple_record" "srv_submission" {
-  domain   = var.domain
-  name     = "_submission._tcp"
-  type     = "SRV"
-  ttl      = var.ttl
-  priority = 0
+resource "dnsimple_zone_record" "srv_submission" {
+  zone_name = var.domain
+  name      = "_submission._tcp"
+  type      = "SRV"
+  ttl       = var.ttl
+  priority  = 0
 
   value = "1 587 smtp.fastmail.com"
 }
 
-resource "dnsimple_record" "srv_imaps" {
-  domain   = var.domain
-  name     = "_imaps._tcp"
-  type     = "SRV"
-  ttl      = var.ttl
-  priority = 0
+resource "dnsimple_zone_record" "srv_imaps" {
+  zone_name = var.domain
+  name      = "_imaps._tcp"
+  type      = "SRV"
+  ttl       = var.ttl
+  priority  = 0
 
   value = "1 993 imap.fastmail.com"
 }
 
-resource "dnsimple_record" "srv_pop3s" {
-  domain   = var.domain
-  name     = "_pop3s._tcp"
-  type     = "SRV"
-  ttl      = var.ttl
-  priority = 0
+resource "dnsimple_zone_record" "srv_pop3s" {
+  zone_name = var.domain
+  name      = "_pop3s._tcp"
+  type      = "SRV"
+  ttl       = var.ttl
+  priority  = 0
 
   value = "1 995 pop.fastmail.com"
 }
 
-resource "dnsimple_record" "srv_jmap" {
-  domain   = var.domain
-  name     = "_jmap._tcp"
-  type     = "SRV"
-  ttl      = var.ttl
-  priority = 0
+resource "dnsimple_zone_record" "srv_jmap" {
+  zone_name = var.domain
+  name      = "_jmap._tcp"
+  type      = "SRV"
+  ttl       = var.ttl
+  priority  = 0
 
   value = "1 443 jmap.fastmail.com"
 }
 
-resource "dnsimple_record" "srv_carddavs" {
-  domain   = var.domain
-  name     = "_carddavs._tcp"
-  type     = "SRV"
-  ttl      = var.ttl
-  priority = 0
+resource "dnsimple_zone_record" "srv_carddavs" {
+  zone_name = var.domain
+  name      = "_carddavs._tcp"
+  type      = "SRV"
+  ttl       = var.ttl
+  priority  = 0
 
   value = "1 443 carddav.fastmail.com"
 }
 
-resource "dnsimple_record" "srv_caldavs" {
-  domain   = var.domain
-  name     = "_caldavs._tcp"
-  type     = "SRV"
-  ttl      = var.ttl
-  priority = 0
+resource "dnsimple_zone_record" "srv_caldavs" {
+  zone_name = var.domain
+  name      = "_caldavs._tcp"
+  type      = "SRV"
+  ttl       = var.ttl
+  priority  = 0
 
   value = "1 443 caldav.fastmail.com"
 }
--- modules/dnsimple-github-pages/main.tf
+++ modules/dnsimple-github-pages/main.tf
@@ -1,15 +1,15 @@
-resource "dnsimple_record" "alias-record-root" {
-  domain = var.domain
-  name   = ""
-  value  = var.gh_pages_url
-  type   = "ALIAS"
-  ttl    = 60
+resource "dnsimple_zone_record" "alias-record-root" {
+  zone_name = var.domain
+  name      = ""
+  value     = var.gh_pages_url
+  type      = "ALIAS"
+  ttl       = 60
 }
 
-resource "dnsimple_record" "cname-record-www" {
-  domain = var.domain
-  name   = "www"
-  value  = var.gh_pages_url
-  type   = "CNAME"
-  ttl    = 60
+resource "dnsimple_zone_record" "cname-record-www" {
+  zone_name = var.domain
+  name      = "www"
+  value     = var.gh_pages_url
+  type      = "CNAME"
+  ttl       = 60
 }
