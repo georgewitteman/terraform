992cdf634f464397b5e2316b69038fc7441bb217
George Witteman <george@witteman.me>
2021-09-26 04:41:12 +0000

    Remove 404 module

--- modules/dnsimple-404/main.tf
+++
@@ -1,8 +0,0 @@
-resource "dnsimple_record" "cname" {
-  domain = var.domain
-  name   = var.subdomain
-  type   = "CNAME"
-  ttl    = var.ttl
-
-  value = "404.al"
-}
--- modules/dnsimple-404/versions.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  required_providers {
-    dnsimple = {
-      source  = "dnsimple/dnsimple"
-      version = "~> 0.5"
-    }
-  }
-  required_version = ">= 1"
-}
