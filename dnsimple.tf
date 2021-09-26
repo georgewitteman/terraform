0f1428cf9b19e155223ce14fecf26b0b4da2a31c
George Witteman <george@witteman.me>
2021-09-26 04:39:54 +0000

    Remove 404.al from witteman.me

--- dnsimple.tf
+++ dnsimple.tf
@@ -7,7 +7,7 @@ module "dnsimple_fastmail_georgewitteman_com" {
 module "dnsimple_fastmail_witteman_me" {
   source    = "./modules/dnsimple-fastmail"
   domain    = "witteman.me"
-  dmarc     = "v=DMARC1; p=quarantine; sp=quarantine; aspf=r; pct=100; rua=mailto:re+igrne0h0z9h@dmarc.postmarkapp.com,mailto:re+1dce928e4bb4@inbound.dmarcdigests.com; ruf=mailto:forensicreports@witteman.me; fo=1:d:s"
+  dmarc     = "v=DMARC1; p=quarantine; sp=quarantine; aspf=r; pct=100; rua=mailto:re+igrne0h0z9h@dmarc.postmarkapp.com; ruf=mailto:forensicreports@witteman.me; fo=1:d:s"
   a_records = "true"
 }
 
@@ -15,10 +15,4 @@ module "dnsimple_github_pages" {
   source       = "./modules/dnsimple-github-pages"
   domain       = "georgewitteman.com"
   gh_pages_url = "georgewitteman.github.io"
-}
-
-module "dnsimple_404_witteman_me" {
-  source    = "./modules/dnsimple-404"
-  domain    = "witteman.me"
-  subdomain = "404"
 }
