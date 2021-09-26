d80be87b84c0d8a931c048fb5634cf321a6e57c1
George Witteman <george@witteman.me>
2021-09-26 05:30:46 +0000

    Upgrade module versions

--- modules/dnsimple-github-pages/versions.tf
+++ modules/dnsimple-github-pages/versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     dnsimple = {
       source  = "dnsimple/dnsimple"
-      version = "~> 0.5"
+      version = "~> 0.9"
     }
   }
   required_version = ">= 1"
--- versions.tf
+++ versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 3.45"
+      version = "~> 3.60"
     }
   }
   required_version = ">= 1"
