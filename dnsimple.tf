8afd3b9501e98707694115180d724bfddf5bfdbd
George Witteman <george@witteman.me>
2021-10-04 02:44:23 +0000

    Revert to free Postmark DMARC reports

--- dnsimple.tf
+++ dnsimple.tf
@@ -1,13 +1,13 @@
 module "dnsimple_fastmail_georgewitteman_com" {
   source = "./modules/dnsimple-fastmail"
   domain = "georgewitteman.com"
-  dmarc  = "v=DMARC1; p=quarantine; sp=quarantine; aspf=r; pct=100; rua=mailto:re+uimzzzgugvk@dmarc.postmarkapp.com; ruf=mailto:forensicreports@witteman.me; fo=1:d:s"
+  dmarc  = "v=DMARC1; p=quarantine; sp=quarantine; aspf=r; pct=100; rua=mailto:re+kwgr6yysiwt@dmarc.postmarkapp.com,mailto:dmarc+rua@witteman.me; ruf=mailto:dmarc+ruf@witteman.me; fo=1:d:s"
 }
 
 module "dnsimple_fastmail_witteman_me" {
   source    = "./modules/dnsimple-fastmail"
   domain    = "witteman.me"
-  dmarc     = "v=DMARC1; p=quarantine; sp=quarantine; aspf=r; pct=100; rua=mailto:re+igrne0h0z9h@dmarc.postmarkapp.com; ruf=mailto:forensicreports@witteman.me; fo=1:d:s"
+  dmarc     = "v=DMARC1; p=quarantine; sp=quarantine; aspf=r; pct=100; rua=mailto:re+etpsv9gfdep@dmarc.postmarkapp.com,mailto:dmarc+rua@witteman.me; ruf=mailto:dmarc+ruf@witteman.me; fo=1:d:s"
   a_records = "true"
 }
 
