50f25a012e4b360760d5ee1e84cc68f2d7778a6f
George Witteman <george@witteman.me>
2021-10-05 15:17:16 +0000

    Add Google Postmaster verification record

--- dnsimple.tf
+++ dnsimple.tf
@@ -10,6 +10,14 @@ module "dnsimple_fastmail_witteman_me" {
   dmarc  = "v=DMARC1; p=quarantine; sp=quarantine; aspf=r; pct=100; rua=mailto:re+etpsv9gfdep@dmarc.postmarkapp.com,mailto:dmarc+rua@witteman.me; ruf=mailto:dmarc+ruf@witteman.me; fo=1:d:s"
 }
 
+resource "dnsimple_zone_record" "google_postmaster_verification" {
+  zone_name = "witteman.me"
+  name      = ""
+  type      = "TXT"
+  ttl       = 3600
+  value     = "google-site-verification=NW7ujmNyY-X8v_VOlYzD3CJGqDD8dsoG8RTgGPMNKtk"
+}
+
 module "dnsimple_github_pages" {
   source       = "./modules/dnsimple-github-pages"
   domain       = "georgewitteman.com"
--- versions.tf
+++ versions.tf
@@ -4,6 +4,10 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 3.60"
     }
+    dnsimple = {
+      source  = "dnsimple/dnsimple"
+      version = "~> 0.9"
+    }
   }
   required_version = ">= 1"
 }
