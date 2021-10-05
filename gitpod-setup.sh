6ddf5b7a036b717cb5f126a97b629201fcd33aec
George Witteman <george@witteman.me>
2021-10-05 01:11:06 +0000

    Install dnsutils in gitpod-setup.sh

--- gitpod-setup.sh
+++ gitpod-setup.sh
@@ -2,5 +2,5 @@
 eval $(gp env --export)
 curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
 sudo apt-add-repository "deb [arch=$(dpkg --print-architecture)] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
-sudo apt install --yes terraform
+sudo apt install --yes terraform dnsutils
 terraform init
