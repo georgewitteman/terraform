29b497bfa7e67041ec05a863599009ca4bb779de
George Witteman <george@witteman.me>
2021-11-04 04:50:11 +0000

    Add email for wtmn.net

--- dnsimple.tf
+++ dnsimple.tf
@@ -1,7 +1,7 @@
 module "dnsimple_fastmail_georgewitteman_com" {
   source = "./modules/dnsimple-fastmail"
   domain = "georgewitteman.com"
-  dmarc  = "v=DMARC1; p=quarantine; sp=quarantine; aspf=r; pct=100; rua=mailto:re+kwgr6yysiwt@dmarc.postmarkapp.com,mailto:dmarc+rua@georgewitteman.com; ruf=mailto:dmarc+ruf@georgewitteman.com; fo=1:d:s"
+  dmarc  = "v=DMARC1; p=quarantine; sp=quarantine; aspf=r; pct=100; rua=mailto:re+kwgr6yysiwt@dmarc.postmarkapp.com,mailto:dmarc+rua@witteman.me; ruf=mailto:dmarc+ruf@witteman.me; fo=1:d:s"
 }
 
 resource "dnsimple_zone_record" "google_postmaster_verification_georgewitteman_com" {
@@ -10,6 +10,12 @@ resource "dnsimple_zone_record" "google_postmaster_verification_georgewitteman_c
   type      = "TXT"
   ttl       = 3600
   value     = "google-site-verification=n-eQPTxgyqAyfJDuPf4xRrL9zZUSGyhZ_PG6wrp7Lic"
+}
+
+module "dnsimple_fastmail_wtmn_net" {
+  source = "./modules/dnsimple-fastmail"
+  domain = "wtmn.net"
+  dmarc  = "v=DMARC1; p=quarantine; sp=quarantine; aspf=r; pct=100; rua=mailto:re+lcidla2subz@dmarc.postmarkapp.com,mailto:dmarc+rua@witteman.me; ruf=mailto:dmarc+ruf@witteman.me; fo=1:d:s"
 }
 
 module "dnsimple_fastmail_witteman_me" {
