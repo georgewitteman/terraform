524777e26059f1cc6bc33b2713e8b17b9509ccec
George Witteman <george@witteman.me>
2021-11-28 17:46:55 +0000

    Resolve code scanning alerts

--- aws.tf
+++ aws.tf
@@ -2,8 +2,38 @@ provider "aws" {
   region = "us-east-1"
 }
 
+resource "aws_s3_bucket" "log_bucket" {
+  bucket = "georgewitteman-terraform-logs"
+  acl    = "log-delivery-write"
+
+  versioning {
+    enabled = true
+  }
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "log_bucket" {
+  bucket                  = aws_s3_bucket.log_bucket.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
 resource "aws_s3_bucket" "terraform_state" {
   bucket = "georgewitteman-terraform-state"
+
+  logging {
+    target_bucket = aws_s3_bucket.log_bucket.id
+    target_prefix = "log/"
+  }
 
   lifecycle {
     prevent_destroy = true
@@ -22,6 +52,18 @@ resource "aws_s3_bucket" "terraform_state" {
   }
 }
 
+resource "aws_s3_bucket_public_access_block" "terraform_state" {
+  bucket                  = aws_s3_bucket.terraform_state.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+resource "aws_kms_key" "dynamo_db_kms" {
+  enable_key_rotation = true
+}
+
 resource "aws_dynamodb_table" "terraform_locks" {
   name         = "terraform-locks"
   billing_mode = "PAY_PER_REQUEST"
@@ -30,5 +72,14 @@ resource "aws_dynamodb_table" "terraform_locks" {
   attribute {
     name = "LockID"
     type = "S"
+  }
+
+  point_in_time_recovery {
+    enabled = true
+  }
+
+  server_side_encryption {
+    enabled     = true
+    kms_key_arn = aws_kms_key.dynamo_db_kms.key_id
   }
 }
