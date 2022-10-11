afa2f02dfe9de77f26d225e75282f7180d4b2f17
George Witteman <george@witteman.me>
2022-10-11 14:02:01 -0700

    Downgrade Ubuntu
    It seems like I might need to set up IPv6. I'm testing to see if downgrading Ubuntu to the version [here](https://github.com/hashicorp/terraform-aws-vault-starter/blob/f763129d4fe2fda9edbe7eef343944ff1a9b1d92/modules/vm/main.tf#L14) works as a temporary fix.

--- modules/aws-vault/ec2.tf
+++ modules/aws-vault/ec2.tf
@@ -6,8 +6,9 @@ data "aws_ami" "ubuntu" {
   most_recent = true
 
   filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+    name = "name"
+    # values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
   }
 
   filter {
