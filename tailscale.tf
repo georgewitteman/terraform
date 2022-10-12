1a7adf21bca3f98fb917415b99808a181da7dcd3
George Witteman <george@witteman.me>
2022-10-11 17:21:55 -0700

    Enable relay to auto approve subnets

--- tailscale.tf
+++ tailscale.tf
@@ -1,9 +1,17 @@
 provider "tailscale" {}
 
+locals {
+  relay_tag = "tag:aws-relay"
+}
+
 resource "tailscale_tailnet_key" "aws_relay" {
   reusable      = true
   ephemeral     = true
   preauthorized = true
+
+  tags = [local.relay_tag]
+
+  depends_on = [tailscale_acl.sample_acl]
 }
 
 module "tailscale_relay" {
@@ -14,4 +22,25 @@ module "tailscale_relay" {
   resource_name_prefix = "main"
   public_subnet_ids    = aws_subnet.public[*].id
   ssh_key_name         = aws_key_pair.georgewitteman.key_name
+}
+
+resource "tailscale_acl" "sample_acl" {
+  acl = jsonencode({
+    acls = [
+      {
+        // Allow all users access to all ports.
+        action = "accept",
+        users  = ["*"],
+        ports  = ["*:*"],
+      }
+    ],
+    tagOwners = {
+      "${local.relay_tag}" = ["autogroup:members"]
+    },
+    autoApprovers = {
+      routes = {
+        "${aws_vpc.this.cidr_block}" = ["${local.relay_tag}"]
+      }
+    }
+  })
 }
