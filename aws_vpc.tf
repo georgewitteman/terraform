d080b07b8d17290789a7c805a078a067730f8e55
George Witteman <george@witteman.me>
2022-11-05 21:39:43 -0700

    Add IPv6 subnets to VPC

--- aws_vpc.tf
+++ aws_vpc.tf
@@ -1,7 +1,56 @@
-resource "aws_vpc" "main" {
-  cidr_block = cidrsubnet("10.0.0.0/8", 8, 0)
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+
+locals {
+  vpc_name = "main"
+
+  vpc_cidr = cidrsubnet("10.0.0.0/8", 8, 0)
+
+  public_subnets  = cidrsubnets(cidrsubnet(aws_vpc.this.ipv6_cidr_block, 1, 0), 7, 7, 7)
+  private_subnets = cidrsubnets(cidrsubnet(aws_vpc.this.ipv6_cidr_block, 1, 1), 7, 7, 7)
+
+  azs = sort(data.aws_availability_zones.available.names)
+}
+
+resource "aws_vpc" "this" {
+  cidr_block = local.vpc_cidr
+
+  assign_generated_ipv6_cidr_block = true
 
   tags = {
-    Name = "main"
+    Name = local.vpc_name
+  }
+}
+
+resource "aws_subnet" "public" {
+  count = length(local.public_subnets)
+
+  vpc_id            = aws_vpc.this.id
+  availability_zone = element(local.azs, count.index)
+  ipv6_cidr_block   = element(local.public_subnets, count.index)
+  ipv6_native       = true
+
+  assign_ipv6_address_on_creation                = true
+  enable_resource_name_dns_aaaa_record_on_launch = true
+
+  tags = {
+    Name = "${local.vpc_name}-public-${element(local.azs, count.index)}"
+  }
+}
+
+resource "aws_subnet" "private" {
+  count = length(local.private_subnets)
+
+  vpc_id            = aws_vpc.this.id
+  availability_zone = element(local.azs, count.index)
+  ipv6_cidr_block   = element(local.private_subnets, count.index)
+  ipv6_native       = true
+
+  assign_ipv6_address_on_creation                = true
+  enable_resource_name_dns_aaaa_record_on_launch = true
+
+  tags = {
+    Name = "${local.vpc_name}-private-${element(local.azs, count.index)}"
   }
 }
