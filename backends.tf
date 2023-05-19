3505c5c9899a48b6dc1ed96480e01d663d0b1bab
George Witteman <george@witteman.me>
2023-05-19 04:38:06 +0000

    Simplify terraform state management

--- backends.tf
+++ backends.tf
@@ -1,9 +1,8 @@
 terraform {
   backend "s3" {
-    bucket         = "georgewitteman-terraform-state"
-    key            = "global/s3/terraform.tfstate"
-    region         = "us-east-1"
-    encrypt        = "true"
-    dynamodb_table = "terraform-locks"
+    bucket  = "georgewitteman-terraform-state-v2"
+    key     = "terraform.tfstate"
+    region  = "us-east-1"
+    encrypt = "true"
   }
 }
--- github.tf
+++ github.tf
@@ -1,9 +1,5 @@
 provider "github" {}
 
-data "github_user" "georgewitteman" {
-  username = "georgewitteman"
-}
-
 data "github_repository" "terraform" {
   full_name = "georgewitteman/terraform"
 }
