3b571496ae4e43151971f53159766f7af4a57eb4
George Witteman <george@witteman.me>
2023-05-19 04:07:27 +0000

    Delete unused AWS resources

--- aws_ec2.tf
+++
@@ -1,6 +0,0 @@
-resource "aws_key_pair" "georgewitteman" {
-  count = length(data.github_user.georgewitteman.ssh_keys)
-
-  key_name   = "georgewitteman-personal-${count.index}"
-  public_key = element(data.github_user.georgewitteman.ssh_keys, count.index)
-}
