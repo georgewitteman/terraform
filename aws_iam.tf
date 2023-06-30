6a2f5e0054ce77c7003320c636e53fed3f6c7f26
George Witteman <george@witteman.me>
2023-06-30 01:36:47 +0000

    Update oidc

--- aws_iam.tf
+++ aws_iam.tf
@@ -7,7 +7,7 @@ resource "aws_iam_openid_connect_provider" "github" {
 
   client_id_list = ["sts.amazonaws.com"]
 
-  thumbprint_list = [data.tls_certificate.github.certificates[0].sha1_fingerprint]
+  thumbprint_list = data.tls_certificate.github.certificates[*].sha1_fingerprint
 }
 
 data "aws_iam_policy_document" "github_oidc" {
